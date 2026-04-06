2050b4d805c2e1b987a700a3a3864218a7b4d278
Austin Young <austineyoung2000@gmail.com>
2026-04-06 14:21:54 -0400

    laguna: Adopts common Google Pixel device configuration
    Removes several custom overlays (GlanceableHub, GoogleConfig, and various PixelConfig overlays) which provided specific configurations and features.
    
    Includes the upstream `device/google/gs-common/device.mk` to leverage official common device configurations for Pixel devices. This streamlines the device setup by relying on a more standard framework for these features.

--- common.mk
+++ common.mk
@@ -11,6 +11,8 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/developer_gsi_keys.mk)
 PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.software.ipsec_tunnel_migration.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.software.ipsec_tunnel_migration.xml
 
+include device/google/gs-common/device.mk
+
 # Modem
 PRODUCT_PACKAGES += dump_modem
 
