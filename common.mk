e6b340b5d9b210040deb704202770e7806bdc8f0
Austin Young <austineyoung2000@gmail.com>
2026-04-11 14:44:06 -0400

    laguna: Disables core GMS components for FULL GMS
    Explicitly sets TARGET_CORE_GMS to false to ship more google default apps.
    
    Change-Id: Ie4c5a0ae5166001be5483184c770d41c81b0c309

--- common.mk
+++ common.mk
@@ -12,6 +12,7 @@ PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.software.ipsec_tunnel_migration.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.software.ipsec_tunnel_migration.xml
 
 include device/google/gs-common/device.mk
+TARGET_CORE_GMS := false
 
 # Modem
 PRODUCT_PACKAGES += dump_modem
